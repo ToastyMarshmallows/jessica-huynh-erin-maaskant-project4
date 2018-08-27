89303852528f42951cbd854b2ddb723443564af6
Erin <erin_maas@yahoo.com>
2018-08-27 00:15:35 -0400

    Adding Anton font as backup

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,161 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const notify = require('gulp-notify');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
-
-gulp.task('styles', () => {
-   return gulp.src('./dev/styles/**/*.scss')
-   .pipe(sass().on('error', sass.logError))
-   .pipe(concat('style.css'))
-   .pipe(gulp.dest('./public/styles'))
-   .pipe(reload({stream:true}));
-});
-
-gulp.task('js', () => {
-   browserify('./dev/scripts/main.js', { debug: true })
-      .transform('babelify', {
-         sourceMaps: true,
-         presets: ['env']
-      })
-      .bundle()
-      .on('error', notify.onError({
-         message: "Error: <%= error.message %>",
-         title: 'Error in JS 💀'
-      }))
-      .pipe(source('main.js'))
-      .pipe(buffer())
-      .pipe(gulp.dest('./public/scripts'))
-      .pipe(reload({stream:true}));
-});
-
-gulp.task('bs', () => {
-   browserSync.init({
-      server: {
-         baseDir:'./'
-      }
-   })
-});
-
-gulp.task('watch',() => {
-   gulp.watch('./dev/styles/**/*.scss', ['styles']);
-   gulp.watch('./dev/scripts/**/*.js', ['js'])
-   gulp.watch('*.html',reload)
-});
-
-gulp.task('default',['bs','styles','js','watch']);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link href="https://fonts.googleapis.com/css?family=Anton|Nunito" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="public/styles/style.css">
+    <title>Overwatch Hero Database</title>
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+            <figure class="OW__Logo">
+                <?xml version="1.0" encoding="utf-8"?>
+                <!-- Generator: Adobe Illustrator 21.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 600 600"
+                    style="enable-background:new 0 0 600 600;" xml:space="preserve">
+                    <!-- <style type="text/css">
+                                    .st0 {
+                                        fill: #4A4C4E;
+                                    }
+                            
+                                    .st1 {
+                                        fill: #FA9C1E;
+                                    }
+                                </style> -->
+                    <g id="svg3960" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                        xmlns:svg="http://www.w3.org/2000/svg">
+                    </g>
+                    <g id="large__bit">
+                        <path id="path4052" class="large__bit" d="M93.6,82.3C33.9,138.3-1.1,219.9,0,302.1c0,85.4,39,169.2,103.7,224.7
+                                    c59.8,52.3,141.1,78.9,220.5,72.2c86-6.4,167.6-52.3,218.1-122.1c49.1-66.3,68.4-153.8,52.1-234.9c-12-62.1-44.5-119.6-90.8-162.4
+                                    l-56.7,65.8c45.7,43,70.6,106.6,65.9,169.2c-2.1,31-11.1,61.7-26.3,89L370.9,292l-58.3-125.6l-0.1,190.2l116.7,112.9
+                                    c-52,40.4-123.4,53.8-186.6,35.9c-25.7-7.2-50.1-19.2-71.3-35.4l117.5-113.4c-0.2-61.8,0.7-128.6,0-190.4L230.5,292L114.1,404.4
+                                    c-35.5-62-36.4-142.2-2.2-204.9c10.7-20.1,24.8-38.4,41.3-54.2L96.5,79.5C95.5,80.4,94.6,81.3,93.6,82.3z"
+                        />
+                    </g>
+                    <g id="small__bit">
+                        <path id="path3961" class="small__bit" d="M296.7,0c-66.8,0.4-132.8,24.2-184.6,66.1l56.7,65.8c47.8-37.7,112.3-53.1,172-41.2
+                                    c32.8,6.3,64.1,20.7,90.5,41.2l56.7-65.8C434.2,22.8,365.8-1,296.7,0z" />
+                    </g>
+                </svg>
+            </figure>
+            <form action="" class="login__container">
+                <input type="text" class="input__bar input__bar--text" placeholder="Admin Login">
+                <input type="submit" class="input__submit input__submit--login">
+            </form>
+            <figure class="OW__Text">
+                    <?xml version="1.0" encoding="utf-8"?>
+                    <!-- Generator: Adobe Illustrator 21.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1500 235"
+                        style="enable-background:new 0 0 1500 235;" xml:space="preserve">
+                        <style type="text/css">
+                            .st0 {
+                                fill: #fff;
+                            }
+                    
+                            .st1 {
+                                fill: #FA9C1E;
+                            }
+                        </style>
+                        <g id="O">
+                            <path id="rect3776" class="O" d="M31.9,54C13.8,54-0.8,65.2-0.8,79.2v75.6c0,9.4,6.6,17.6,16.5,21.9L43,149.4v-3.8V88h78.4v57.7
+                            h-57l-34.3,34.3c0.6,0,1.2,0,1.9,0h99.6c18.1,0,32.8-11.2,32.8-25.2V86.8l-19.3-30.7c-4.1-1.4-8.6-2.2-13.4-2.2
+                            C131.5,54,31.9,54,31.9,54z" />
+                        </g>
+                        <g id="V">
+                            <path id="path3863" class="V" d="M159.3,54l79.6,126h15.7h15.7l79.6-126h-48.5l-46.8,74.9L207.8,54L159.3,54L159.3,54z" />
+                        </g>
+                        <g id="triangle_x5F__x5F_1">
+                            <path id="path4072" class="triangle" d="M219.6,54l35-51.6l35,51.6h-35H219.6z" />
+                        </g>
+                        <g id="E">
+                            <path id="rect3885" class="E" d="M367.1,54l-20.5,32.5V180h133v-34.3h-29.5h-60.5v-14.4h60.5v-28.9h-60.5V88h90V54H367.1
+                            L367.1,54z" />
+                        </g>
+                        <g id="R">
+                            <path id="rect3950" class="R" d="M496.4,54v22.7v32.5V180h43.5v-34.3v-13.8V88h18.4H606v18.6h-47.7v25.2l52.8,48.2h58.6
+                            l-52.8-48.2h4.9c14.5,0,26.2-10.1,26.2-22.7V76.7c0-12.6-11.7-22.7-26.2-22.7h-81.9h-17.3L496.4,54L496.4,54z" />
+                        </g>
+                        <g id="W">
+                            <path id="path3982" class="W" d="M655.2,54l55.2,126h27.5l28.8-62.1l27.4,62.1h30l55.3-126h-43l-26.2,63l-27.8-63h-15.7h-12.9
+                            l-27.8,63l-26.5-63L655.2,54L655.2,54z" />
+                        </g>
+                        <g id="A">
+                            <path id="path3863-2" class="A" d="M843.8,180l83.3-126h16.4H960l83.3,126h-50.8l-49-74.9l-49,74.9H843.8z" />
+                        </g>
+                        <g id="triangle_x5F__x5F_2">
+                            <path id="path4072-1" class="triangle" d="M908.6,180l35,51.6l35-51.6h-35H908.6z" />
+                        </g>
+                        <g id="T">
+                            <path id="rect4003" class="T" d="M1009.1,54v34h54.1v92h43.1V88h42.8l22.6-34L1009.1,54L1009.1,54z" />
+                        </g>
+                        <g id="C">
+                            <path id="rect3776-0" class="C" d="M1195.5,54c-1.7,0-3.3,0.1-4.9,0.3l-27.8,42.1v58.5c0,14,14.6,25.2,32.8,25.2h90.7
+                            c18.1,0,32.8-11.2,32.8-25.2v-25.9h-43.7v16.7h-68.3V88h68.3v14.4h43.7V79.2c0-14-14.6-25.2-32.8-25.2L1195.5,54L1195.5,54z"
+                            />
+                        </g>
+                        <g id="H" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                            xmlns:svg="http://www.w3.org/2000/svg">
+                            <path id="rect4024" class="H" d="M1335.6,54v126h44.2v-48.4h74.7V180h43.5V54h-43.5v44.3h-74.7V54H1335.6z" />
+                        </g>
+                    </svg>
+            </figure>
+            <h1 class="visuallyhidden">Overwatch Hero Database</h1>
+            <a href="index.html"><i class="fas fa-sign-out-alt sign__out"></i></a>
+        </div>
+    </header>
+        <main>
+            <aside>
+                <div class="search__wrapper">
+                    <form action="" class="search__container">
+                        <input type="search" placeholder="enter hero name" class="input__bar input__bar--search">
+                        <input type="submit" value="Search" class="input__submit input__submit--search">
+                    </form>
+                </div>
+                <section class="search__results__heading">
+                    <h3>Results Found:</h3>
+                </section>
+                <section class="search__results__card">
+                    <figure class="hero__card">
+                        <figcaption class="hero__card__fig">
+                            <!-- <h4 class="hero__card__heading"></h4> -->
+                        </figcaption>
+                    </figure>
+                    <h4 class="hero__card__heading"></h4>
+                </section>
+            </aside>
+    
+            <section class="hero__container">
+                <figure class="hero__image">
+    
+                </figure>
+                        <ul class="hero__info">
+            
+                        </ul>
+                <div id="map">
+            
+                </div>
+            </section>
+    
+        </main>
+    <footer class="visuallyhidden">
+        <div class="credit">
+            We don't own any of these images and could not have made this project without the art sourced from different artists:
+            Hero Images were obtained via http://overwatch.wikia.com/wiki/Overwatch_Wiki
+            and some background images were obtained from:
+            https://www.reddit.com/r/Overwatch/comments/6cjw2p/nepal_map_view_from_the_sombra_bug_edited_into_a/ 
+            https://blisschen.tumblr.com/
+            http://www.hdwallpaperspulse.com/free-background-2.html
+        </div>
+    </footer>
+    <script src='https://code.jquery.com/jquery-3.2.1.min.js' integrity='sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4='crossorigin='anonymous'></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCeTIiHDXlITR2WjpJlf-1AigrB7Tl_r5U">
+    </script>
+    <script src="dev/scripts/main.js"></script>
+</body>
+</html>
